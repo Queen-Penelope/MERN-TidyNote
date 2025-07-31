f6d7a33b7318f454378b7a141d68999d6b32f004
Rym Bachrouch <icanseeyou9905@gmail.com>
2025-07-31 15:04:18 +0100

    prepared for render

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,15 +4,22 @@ import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./lib/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import cors from "cors";
+import path from "path";
+
 const app = express();
 
 dotenv.config();
 const port = process.env.PORT;
+const __dirname = path.resolve();
+
 
 //MIDDLEWARE
-app.use(cors({
+if(process.env.NODE.ENV !== "production") {
+  app.use(cors({
   origin: "http://localhost:5173",
-}))
+}));;
+}
+
 app.use(express.json()); //get access to req.body
 app.use(rateLimiter);
 
@@ -20,6 +27,13 @@ app.use(rateLimiter);
 //Routes
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(port, () => {
